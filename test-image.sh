10c7cab2f01ba786fea8888246d0ab200477352f
Mario Inga <mario21ic@gmail.com>
2021-02-28 03:13:14 -0500

    adding container-structure-test example

---
+++ test-image.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Project https://github.com/GoogleContainerTools/container-structure-test
+
+container-structure-test test --image mario21ic/api-demo:latest --config container-structure-test.yaml
